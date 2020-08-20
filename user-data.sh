3eefc38262e5da10d3ce89a945d945994443b9d1
cesar-barahona <cesar.barahona@laureate.net>
2020-08-19 20:57:40 -0600

    become user ubuntu

--- user-data.sh
+++ user-data.sh
@@ -6,7 +6,8 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt install python3.8 --yes
 apt install git --yes
 apt install ansible --yes
+cd /home/ubuntu
 mkdir /home/ubuntu/app
 git clone git://github.com/CEBS13/node-ansible
-cd node-ansible
-ansible-playbook node-playbook.yml
+#cd node-ansible
+#ansible-playbook node-playbook.yml
